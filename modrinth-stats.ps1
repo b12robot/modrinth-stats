84869ac1302de6542d0f32d22d159ebb33381f7a
B12 <87882740+b12robot@users.noreply.github.com>
2026-07-16 17:20:34 +0300

    Update modrinth-stats.ps1

--- modrinth-stats.ps1
+++ modrinth-stats.ps1
@@ -1,23 +1,24 @@
-﻿[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+﻿# Config
+[CmdletBinding()]
+param (
+    [string[]]$Versions = @(
+        "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
+        "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6","1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11",
+        "26.1", "26.1.1", "26.1.2", "26.2"
+    ),
+
+    [string[]]$Loaders = @(
+        "forge", "fabric", "neoforge", "quilt"
+    ),
+
+    [int]$ValidHours = 48
+)
+
+# Initialization
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $Host.UI.RawUI.WindowTitle = "Modrinth Stats"
 $ProgressPreference = 'SilentlyContinue'
-
-$Versions = @(
-	"1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.16.5",
-	"1.17", "1.17.1",
-	"1.18", "1.18.1", "1.18.2",
-	"1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4",
-    "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
-    "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6","1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11",
-	"26.1", "26.1.1", "26.1.2"
-)
-
-$Loaders = @(
-	"forge", "fabric", "neoforge", "quilt"
-)
-
-$ValidHours = 48
-$CachePath = Join-Path -Path $env:USERPROFILE -ChildPath "mod_stats_cache.json"
+$CachePath = Join-Path -Path $env:USERPROFILE -ChildPath "modrinth-stats-cache.json"
 $LastWriteTime = [datetime]::MinValue
 $Data = $null
 
@@ -35,8 +36,8 @@ if (Test-Path -Path $CachePath) {
 
 $MaxVersionLength = 0
 foreach ($Version in $Versions) {
-    if ($Version.Length -gt $MaxVersionLength) { 
-        $MaxVersionLength = $Version.Length 
+    if ($Version.Length -gt $MaxVersionLength) {
+        $MaxVersionLength = $Version.Length
     }
 }
 
@@ -47,16 +48,23 @@ foreach ($Loader in $Loaders) {
 	}
 }
 
-# Fetch data from API.
+$RequestCount = 0
+$TotalRequests = $Versions.Count * $Loaders.Count
 $CurrentTime = (Get-Date).ToUniversalTime()
 $TimeDifference = ($CurrentTime - $LastWriteTime).TotalHours
 
-if (-not $Data -or $Data.Count -ne $Versions.Count -or $TimeDifference -gt $ValidHours) {
+# Fetch data from API.
+if ($TimeDifference -gt $ValidHours -or
+	-not $Data -or
+	$Data.Count -ne $Versions.Count -or
+	@($Data[0].Loaders.PSObject.Properties).Count -ne $Loaders.Count) {
 	$Data = [System.Collections.Generic.List[object]]::new()
 	foreach ($Version in $Versions) {
 		$Stats = @{}
 		foreach ($Loader in $Loaders) {
 			$Delay = 500
+			$RequestCount++
+			$Percentage = [math]::Round(($RequestCount / $TotalRequests) * 100)
 			try {
 				$Facets = "[[`"versions:$Version`"],[`"categories:$Loader`"]]"
 				$Url = "https://api.modrinth.com/v2/search?project_type=mod&facets=$Facets&limit=1"
@@ -66,7 +74,7 @@ if (-not $Data -or $Data.Count -ne $Versions.Count -or $TimeDifference -gt $Vali
 				$Limit = [int]$Response.Headers["X-Ratelimit-Limit"]
 				$Remaining = [int]$Response.Headers["X-Ratelimit-Remaining"]
 				$Reset = [int]$Response.Headers["X-Ratelimit-Reset"]
-				Write-Host ("[INFO] {0,-$MaxVersionLength} │ {1,-$MaxLoaderLength} -> {2,-6} │ Limit: {3,-4} │ Remaining: {4,-4} │ Reset: {5,-4}" -f $Version, $Loader, $Hits, $Limit, $Remaining, $Reset) -ForegroundColor Cyan
+				Write-Host ("[INFO] [{0,3}/{1}] {2,3}% │ {3,-$MaxVersionLength} │ {4,-$MaxLoaderLength} -> {5,-6} │ Limit: {6,-4} │ Remaining: {7,-4} │ Reset: {8,-4}" -f $RequestCount, $TotalRequests, $Percentage, $Version, $Loader, $Hits, $Limit, $Remaining, $Reset) -ForegroundColor Cyan
 				$Threshold = [math]::Max([int]($Limit * 0.1), 4)
 				if ($Remaining -lt $Threshold -and $Remaining -gt 0) {
 					$DynamicDelay = ($Reset / $Remaining) * 1000
@@ -126,16 +134,20 @@ $BotBorder = "╚" + ($Segments -join "╩") + "╝"
 
 # Calculate percentage.
 $AllHits = $(
-	foreach ($Item in $Data) {
-		foreach ($Loader in $Loaders) {
-			[int]$Item.Loaders.$Loader
-		}
-	}
+    foreach ($Item in $Data) {
+        foreach ($Loader in $Loaders) {
+            $Value = [int]$Item.Loaders.$Loader
+            if ($Value -ge 0) { $Value }
+        }
+    }
 ) | Sort-Object
 
-$P25 = $AllHits[[int]($AllHits.Count * 0.25)]
-$P50 = $AllHits[[int]($AllHits.Count * 0.50)]
-$P75 = $AllHits[[int]($AllHits.Count * 0.75)]
+if ($AllHits.Count -gt 0) {
+	$P20 = $AllHits[[int]($AllHits.Count * 0.20)]
+	$P40 = $AllHits[[int]($AllHits.Count * 0.40)]
+	$P60 = $AllHits[[int]($AllHits.Count * 0.60)]
+	$P80 = $AllHits[[int]($AllHits.Count * 0.80)]
+}
 
 # Set color palette.
 $Esc = [char]27
@@ -149,11 +161,13 @@ $ColorBorder  = "$Esc[38;2;118;118;118m" # Campbell Bright Black
 $ColorVersion = "$Esc[38;2;225;230;235m" # Crisp White
 $ColorArrow   = "$Esc[38;2;90;140;180m"  # Slate Blue
 $ColorRow     = "$Esc[48;2;22;27;34m"    # Dark Blue Gray
+$ColorError   = "$Esc[38;2;180;50;210m"  # Galactic Purple
 
 # Heatmap
-$ColorHitP25  = "$Esc[38;2;70;190;255m"  # Bright Cyan Blue
-$ColorHitP50  = "$Esc[38;2;240;210;90m"  # Warm Sun Yellow
-$ColorHitP75  = "$Esc[38;2;250;130;50m"  # Vibrant Tangerine
+$ColorHitP20  = "$Esc[38;2;20;100;250m"  # Deep Blue
+$ColorHitP40  = "$Esc[38;2;70;190;255m"  # Bright Cyan Blue
+$ColorHitP60  = "$Esc[38;2;240;210;90m"  # Warm Sun Yellow
+$ColorHitP80  = "$Esc[38;2;250;130;50m"  # Vibrant Tangerine
 $ColorHitP100 = "$Esc[38;2;240;70;70m"   # Crimson Red
 
 # Render the table.
@@ -175,9 +189,20 @@ for ($RowIndex = 0; $RowIndex -lt $Data.Count; $RowIndex++) {
     $RowOutput = foreach ($Loader in $Loaders) {
         $CellData = $GroupedData[$Loader][$RowIndex]
 		$Hit = [int]$CellData.Hits
-		$ColorHit = if ($Hit -lt $P25) { $ColorHitP25 } elseif ($Hit -lt $P50) { $ColorHitP50 } elseif ($Hit -lt $P75) { $ColorHitP75 } else { $ColorHitP100 }
-        $VersionText = ("{0,-$MaxVersionLength}" -f $CellData.Version)
-        $HitsText = ("{0,$MaxHitsLength}" -f $Hit)
+		if ($Hit -lt 0) {
+			$ColorHit = $ColorError
+			$DisplayHit = "NA"
+		}
+		else {
+			$ColorHit = if ($Hit -lt $P20) { $ColorHitP20 }
+				elseif ($Hit -lt $P40) { $ColorHitP40 }
+				elseif ($Hit -lt $P60) { $ColorHitP60 }
+				elseif ($Hit -lt $P80) { $ColorHitP80 }
+				else { $ColorHitP100 }
+			$DisplayHit = $Hit
+		}
+		$VersionText = "{0,-$MaxVersionLength}" -f $CellData.Version
+		$HitsText = "{0,$MaxHitsLength}" -f $DisplayHit
         " $ColorVersion$VersionText $ColorArrow-> $ColorHit$HitsText$ColorDefault "
     }
 	$RowText = $RowOutput -join "$ColorBorder║"
